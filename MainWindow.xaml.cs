70e2390affeca69cc95dcffbac48c2603c45e2a7
patharapol <76545029+patharapol@users.noreply.github.com>
2021-01-26 22:13:20 +0700

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            sum();
+
+            txtday.Text= sum().ToString();
+
+        }
+        private  int sum()
+        {
+            int receive = int.Parse(txtrece.Text);
+            int pay = int.Parse(txtpay.Text);
+            int item = int.Parse(txtitem.Text);
+            int day =  item/(receive - pay);
+
+            return day;
+
+        }
+    }
+}
